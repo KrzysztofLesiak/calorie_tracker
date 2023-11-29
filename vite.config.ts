88990e758cf7ce9602d77ce63ba1fc8c0367af37
Krzysztof <krzysztoflesiak99@gmail.com>
2023-11-29 22:23:41 +0100

    FEAT/Add-jest

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-// export default ({ mode }) => {
-//   process.env = Object.assign(process.env, loadEnv(mode, process.cwd()));
-
-//   return defineConfig({
-//     plugins: [react(), svgr()],
-//   });
-// };
-
 export default defineConfig(() => {
   const env = loadEnv("mock", process.cwd(), "");
   const processEnvValues = {
